ff2c03cf3cab8719bc3b23df2cd4da0982199408
Robert Winkler <rwinkler@antmicro.com>
2019-11-12 14:45:19 +0100

    Add Popcount CFU to picorv32

---
+++ cfu.vh
@@ -0,0 +1,22 @@
+// Copyright (C) 2019, Gray Research LLC
+
+`define vp /* verilator public */
+`define vloff_width /* verilator lint_off WIDTH */
+`define vlon_width /* verilator lint_on WIDTH */
+
+`define CFU_INTERFACE_ID    [CFU_INTERFACE_ID_W-1:0]
+`define CFU_FUNCTION_ID     [CFU_FUNCTION_ID_W-1:0]
+`define CFU_REORDER_ID      [CFU_REORDER_ID_W-1:0]
+`define CFU_REQ_RESP_ID     [CFU_REQ_RESP_ID_W-1:0]
+`define CFU_REQ_DATA        [CFU_REQ_DATA_W-1:0]
+`define CFU_RESP_DATA       [CFU_RESP_DATA_W-1:0]
+`define CFU_ERROR_ID        [CFU_ERROR_ID_W-1:0]
+
+`define CFU_DATA            [CFU_RESP_DATA_W-1:0]
+
+`define CFU_L0_PARAMETERS(INPUTS,DEF_WIDTH) \
+    parameter CFU_FUNCTION_ID_W = 16, \
+    parameter CFU_REQ_INPUTS = INPUTS, \
+    parameter CFU_REQ_DATA_W = DEF_WIDTH, \
+    parameter CFU_RESP_OUTPUTS = 1, \
+    parameter CFU_RESP_DATA_W = DEF_WIDTH
--- picorv32.v
+++ picorv32.v
@@ -29,6 +29,9 @@
 // `define DEBUGASM
 // `define DEBUG
 
+/* Defines for CFU */
+`include "cfu.vh"
+
 `ifdef DEBUG
   `define debug(debug_command) debug_command
 `else
@@ -268,6 +271,19 @@ module picorv32 #(
 	reg [31:0] pcpi_int_rd;
 	reg        pcpi_int_wait;
 	reg        pcpi_int_ready;
+
+	picorv32_pcpi_pop pcpi_pop (
+			.clk       (clk            ),
+			.resetn    (resetn         ),
+			.pcpi_valid(pcpi_valid     ),
+			.pcpi_insn (pcpi_insn      ),
+			.pcpi_rs1  (pcpi_rs1       ),
+			.pcpi_rs2  (pcpi_rs2       ),
+			.pcpi_wr   (pcpi_mul_wr    ),
+			.pcpi_rd   (pcpi_mul_rd    ),
+			.pcpi_wait (pcpi_mul_wait  ),
+			.pcpi_ready(pcpi_mul_ready )
+	);
 
 	generate if (ENABLE_FAST_MUL) begin
 		picorv32_pcpi_fast_mul pcpi_mul (
@@ -2804,6 +2820,54 @@ endmodule
 /***************************************************************
  * picorv32 CFU (Multiply And Accumulate)
  ***************************************************************/
+/*
+module picorv32_pcpi_mul (
+    parameter CFU_VERSION = 0,
+    parameter CFU_FUNCTION_ID_W = 1,
+    parameter CFU_REQ_RESP_ID_W = 6,
+    parameter CFU_REQ_DATA_W = 32,
+    parameter CFU_RESP_DATA_W = CFU_REQ_DATA_W,
+    parameter CFU_ERROR_ID_W = CFU_RESP_DATA_W
+) (
+	input             clk, reset,
+	input             pcpi_valid,
+	input      [31:0] pcpi_insn,
+	input      [31:0] pcpi_rs1,
+	input      [31:0] pcpi_rs2,
+	output reg        pcpi_wr,
+	output reg [31:0] pcpi_rd,
+	output reg        pcpi_wait,
+	output reg        pcpi_ready
+);
+
+	reg [1:0][31:0] req_data_comb,
+	assign data_comb[0] = pcpi_rs1;
+	assign data_comb[1] = pcpi_rs2;
+
+	MulAcc_PipeCFU #(
+		.CFU_FUNCTION_ID_W    (1),
+		.CFU_REQ_RESP_ID_W    (CFU_REQ_RESP_ID_W),
+		.CFU_REQ_INPUTS       (2),
+		.CFU_REQ_DATA_W       (CFU_REQ_DATA_W),
+		.CFU_RESP_OUTPUTS     (1),
+		.CFU_RESP_DATA_W      (CFU_RESP_DATA_W),
+		.CFU_ERROR_ID_W       (CFU_RESP_DATA_W),
+		.CFU_LATENCY          (3)
+	) mulacc(
+		.clock                (clk),
+		.reset                (reset),
+		.clock_en             (1'b1),
+		.req_valid            (pcpi_valid),
+		.req_function_id      (1'b1),
+		.req_id               (1'b0),
+		.req_data             (data_comb),
+		.resp_valid           (),
+		.resp_id              (),
+		.resp_data            (),
+		.resp_ok              (),
+		.resp_error_id        ()
+	);
+endmodule
 
 // Level-1 (Pipelined) multiply-accumulate (cumulative dot product)
 //
@@ -2812,10 +2876,10 @@ endmodule
 //
 // Metadata
 //  Supports: REQ_WIDTH==32 or REQ_WIDTH==64
-//  stateful interface /*IID_IMulAcc*/ IMulAcc {
+//  stateful interface // IID_IMulAcc // IMulAcc {
 //      int acc;
-//      /*0*/ reset(a,b) { return acc = 0; }
-//      /*1*/ mulacc(a,b) { return acc += a*b; }
+//      // 0 // reset(a,b) { return acc = 0; }
+//      // 1 // mulacc(a,b) { return acc += a*b; }
 //  }
 //  II=1
 //  Latency=3
@@ -2897,7 +2961,132 @@ module MulAcc_PipeCFU #(
 	assign resp_ok = 1;
 	assign resp_error_id = 0;
 endmodule
+*/
 
+/***************************************************************
+ * picorv32_wb
+ ***************************************************************/
+
+module picorv32_pcpi_pop #(
+    parameter CFU_REQ_DATA_W   = 32,
+    parameter CFU_RESP_DATA_W  = CFU_REQ_DATA_W
+) (
+	input             clk, resetn,
+	input             pcpi_valid,
+	input      [31:0] pcpi_insn,
+	input      [31:0] pcpi_rs1,
+	input      [31:0] pcpi_rs2,
+	output reg        pcpi_wr,
+	output reg [31:0] pcpi_rd,
+	output reg        pcpi_wait,
+	output reg        pcpi_ready
+);
+	reg [31:0] result;
+
+	Popcount32_CombCFU #(
+		.CFU_FUNCTION_ID_W      (1),
+		.CFU_REQ_INPUTS         (1),
+		.CFU_REQ_DATA_W         (CFU_REQ_DATA_W),
+		.CFU_RESP_OUTPUTS       (1),
+		.CFU_RESP_DATA_W        (CFU_RESP_DATA_W)
+	) pc (
+		.req_function_id        (1'b0),
+		.req_data               (req_rs1),
+		.resp_data              (result)
+	);
+
+	always @(posedge clk) begin
+		if (resetn) begin
+			if (pcpi_insn[14:12]) begin
+				pcpi_rd <= result;
+				pcpi_wr <= 1;
+				pcpi_ready <= 1;
+			end
+		end
+	end
+
+endmodule
+
+
+// Level-0 (Combinational) 32b popcount CFU
+//
+// Metadata
+//  IID=IID_Popcount32
+//  Functions={Popcount32}
+//  Inputs=1x32b
+//  Outputs=1x32b
+//
+module Popcount32_CombCFU #(
+    `CFU_L0_PARAMETERS(1,32)
+) (
+    input `CFU_FUNCTION_ID req_function_id,
+    input [0:0]`CFU_REQ_DATA req_data,
+    output [0:0]`CFU_RESP_DATA resp_data
+);
+    wire [5:0] popcount;
+    Popcount32 popcount_(.i(req_data[0]), .popcount);
+    assign resp_data[0] = {26'b0,popcount};
+
+    // assert(req_function_id == IID_Popcount32.Popcount32)
+    wire _unused_ok = &{1'b0,req_function_id,1'b0};
+endmodule
+
+//////////////////////////////////////////////////////////////////////////////
+// 32-bit population count
+//
+// Per the blog post "Quick FPGA Hacks: Population count"
+// [http://fpga.org/2014/09/05/quick-fpga-hacks-population-count/]
+//
+module Popcount32(
+    input [31:0] i,
+    output [5:0] popcount);  // # of 1's in i
+
+    wire [2:0] c0500, c1106, c1712, c2318, c2924, c3130, c0, c1, c2;
+
+    // add six bundles of six bits
+    C63 c0500_(.i(i[ 5: 0]), .o(c0500));
+    C63 c1106_(.i(i[11: 6]), .o(c1106));
+    C63 c1712_(.i(i[17:12]), .o(c1712));
+    C63 c2318_(.i(i[23:18]), .o(c2318));
+    C63 c2924_(.i(i[29:24]), .o(c2924));
+    C63 c3130_(.i({4'b0,i[31:30]}), .o(c3130));
+
+    // sum the bits set in the [0], [1], and [2] bit positions separately
+    C63 c0_(.i({c0500[0],c1106[0],c1712[0],c2318[0],c2924[0],c3130[0]}), .o(c0));
+    C63 c1_(.i({c0500[1],c1106[1],c1712[1],c2318[1],c2924[1],c3130[1]}), .o(c1));
+    C63 c2_(.i({c0500[2],c1106[2],c1712[2],c2318[2],c2924[2],c3130[2]}), .o(c2));
+
+    assign popcount = {2'b0,c0} + {1'b0,c1,1'b0} + {c2,2'b0};
+endmodule
+
+//////////////////////////////////////////////////////////////////////////////
+// 6:3 compressor as a 64x3b ROM -- three 6-LUTs
+//
+module C63(
+    input [5:0] i,
+    output reg [2:0] o);    // # of 1's in i
+
+    always @* begin
+        case (i)
+        6'h00: o = 0; 6'h01: o = 1; 6'h02: o = 1; 6'h03: o = 2;
+        6'h04: o = 1; 6'h05: o = 2; 6'h06: o = 2; 6'h07: o = 3;
+        6'h08: o = 1; 6'h09: o = 2; 6'h0A: o = 2; 6'h0B: o = 3;
+        6'h0C: o = 2; 6'h0D: o = 3; 6'h0E: o = 3; 6'h0F: o = 4;
+        6'h10: o = 1; 6'h11: o = 2; 6'h12: o = 2; 6'h13: o = 3;
+        6'h14: o = 2; 6'h15: o = 3; 6'h16: o = 3; 6'h17: o = 4;
+        6'h18: o = 2; 6'h19: o = 3; 6'h1A: o = 3; 6'h1B: o = 4;
+        6'h1C: o = 3; 6'h1D: o = 4; 6'h1E: o = 4; 6'h1F: o = 5;
+        6'h20: o = 1; 6'h21: o = 2; 6'h22: o = 2; 6'h23: o = 3;
+        6'h24: o = 2; 6'h25: o = 3; 6'h26: o = 3; 6'h27: o = 4;
+        6'h28: o = 2; 6'h29: o = 3; 6'h2A: o = 3; 6'h2B: o = 4;
+        6'h2C: o = 3; 6'h2D: o = 4; 6'h2E: o = 4; 6'h2F: o = 5;
+        6'h30: o = 2; 6'h31: o = 3; 6'h32: o = 3; 6'h33: o = 4;
+        6'h34: o = 3; 6'h35: o = 4; 6'h36: o = 4; 6'h37: o = 5;
+        6'h38: o = 3; 6'h39: o = 4; 6'h3A: o = 4; 6'h3B: o = 5;
+        6'h3C: o = 4; 6'h3D: o = 5; 6'h3E: o = 5; 6'h3F: o = 6;
+        endcase
+    end
+endmodule
 /***************************************************************
  * picorv32_wb
  ***************************************************************/
