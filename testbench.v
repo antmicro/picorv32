e006cb914affde4101f07acb37f932650daa4d9e
Robert Winkler <rwinkler@antmicro.com>
2019-11-13 16:06:01 +0100

    Remove unnecessary display

--- testbench.v
+++ testbench.v
@@ -51,10 +51,6 @@ module testbench #(
 		end
 	end
 
-	initial begin
-		$display("Testing picorv32 with Icarus using picorv wrapper");
-	end
-
 	picorv32_wrapper #(
 		.AXI_TEST (AXI_TEST),
 		.VERBOSE  (VERBOSE)
